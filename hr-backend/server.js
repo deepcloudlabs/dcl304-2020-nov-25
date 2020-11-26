85713f80b48527c79f65d397fe4e1b6a4e063c98
deepcloudlabs <deepcloudlabs@gmail.com>
2020-11-26 16:01:49 +0300

    add initial employee schema add utility for validation

--- hr-backend/server.js
+++ hr-backend/server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let bodyParser = require("body-parser");
 let logger = require("morgan");
+let mongoose = require("mongoose");
 
 const port = 9001;
 const api = express();
@@ -12,6 +13,37 @@ api.use((req, res, next) => { // CORS
     res.header("Access-Control-Allow-Methods", "HEAD, POST, PUT, GET, DELETE, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
+// Resource -> Aggregate -> Entity (Employee) -> Database
+const employeeSchema = new mongoose.Schema({
+    "_id": {
+        type: String,
+        required: true
+    },
+    "fullname": {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    identityNo: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [
+            , 'You must provide a valid identity no'
+        ]
+    }
+
+});
+
+// REST over HTTP -> Integration Technology
+// Single Business Capability -> MSA -> HR
+// RESTful Service -> Single Business Capability ->
+// Spring, .Net Core, Angular/React/Vue, Golang, Python, ...
+// Resource: DDD -> Aggregate: Employee
+// Hire Employee -> POST Employee
+// Fire Employee -> DELETE Employee
+// Increase Salary -> PUT/PATCH Employee
+// Query Employee -> GET Identity/Department/...
 
 let server = api.listen(port);
 console.log("REST Api is up and running @ " + port);
