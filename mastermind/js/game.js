565506b252975fc676a512cefeb6e6c15bc25c79
deepcloudlabs <deepcloudlabs@gmail.com>
2020-11-26 10:13:47 +0300

    add Move class add createMessage method

--- mastermind/js/game.js
+++ mastermind/js/game.js
@@ -1,3 +1,10 @@
+class Move {
+    constructor(guess, message) {
+        this.guess = guess;
+        this.message = message;
+    }
+}
+
 class GameViewModel {
     constructor() {
         this.tries = 0;
@@ -6,6 +13,18 @@ class GameViewModel {
         this.secret = this.createSecret();
         this.createSecret = this.createSecret.bind(this);
         this.createDigit = this.createDigit.bind(this);
+    }
+
+    play(guess) {
+        guess = Number(guess) || this.createSecret();
+        if (guess === this.secret) {
+            this.level++;
+            this.initGame();
+        } else {
+            this.tries++;
+            let message = this.createMessage(guess);
+            this.moves.push(new Move(guess, message));
+        }
     }
 
     createSecret() {
@@ -22,5 +41,37 @@ class GameViewModel {
 
     createDigit(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    initGame() {
+        this.moves = [];
+        this.tries = 0;
+        this.secret = this.createSecret();
+    }
+
+    createMessage(guess) {
+        let guessAsString = guess.toString();
+        let secretAsString = this.secret.toString();
+        let perfectMatch = 0, partialMatch = 0;
+        for (let i = 0; i < guessAsString.length; i++) {
+            let g = guessAsString.charAt(i);
+            for (let j = 0; j < secretAsString.length; j++) {
+                let s = secretAsString.charAt(j);
+                if (g === s) {
+                    if (i === j)
+                        perfectMatch++;
+                    else
+                        partialMatch++;
+                }
+            }
+        }
+        if (perfectMatch === 0 && partialMatch === 0)
+            return "No match";
+        let message = "";
+        if (partialMatch > 0)
+            message = "-" + partialMatch;
+        if (perfectMatch > 0)
+            message += "+" + perfectMatch;
+        return message;
     }
 }
