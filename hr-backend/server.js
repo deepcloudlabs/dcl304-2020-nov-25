32f308bee1fd100007fe457fefd1e883c4ffebd4
deepcloudlabs <deepcloudlabs@gmail.com>
2020-11-26 16:14:46 +0300

    update employee schema

--- hr-backend/server.js
+++ hr-backend/server.js
@@ -3,6 +3,39 @@ let bodyParser = require("body-parser");
 let logger = require("morgan");
 let mongoose = require("mongoose");
 
+tcKimlikNoValidator = value => {
+    if (value.match("^\\d{11}$") == null) {
+        return false;
+    }
+    let digits = new Array(11);
+    for (let i = 0; i < digits.length; ++i) {
+        digits[i] = value.charCodeAt(i) - 48;
+        if (digits[i] < 0 || digits[i] > 9) {
+            return false;
+        }
+    }
+    let x = digits[0];
+    let y = digits[1];
+    for (let i = 1; i < 5; i++) {
+        x += Number(digits[2 * i]);
+    }
+    for (let i = 2; i <= 4; i++) {
+        y += Number(digits[2 * i - 1]);
+    }
+    let c1 = 7 * x - y;
+    if (c1 % 10 != digits[9]) {
+        return false;
+    }
+    let c2 = 0;
+    for (let i = 0; i < 10; ++i) {
+        c2 += digits[i];
+    }
+    if (c2 % 10 != digits[10]) {
+        return false;
+    }
+    return true;
+};
+
 const port = 9001;
 const api = express();
 
@@ -29,10 +62,38 @@ const employeeSchema = new mongoose.Schema({
         required: true,
         unique: true,
         validate: [
-            , 'You must provide a valid identity no'
+            tcKimlikNoValidator , 'You must provide a valid identity no'
         ]
+    },
+    photo : {
+        type: String,
+        required: false
+    },
+    iban : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    birthYear: {
+        type: Number,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true,
+        min: 3500
+    },
+    fulltime: {
+        type: Boolean,
+        required: false,
+        default: true
+    },
+    department: {
+        type: String,
+        enum: ["IT", "Sales", "Finance", "HR"],
+        required: false,
+        default: "Finance"
     }
-
 });
 
 // REST over HTTP -> Integration Technology
