0ba8603f9631c1caea69c554c8de5fd53bf5f63f
deepcloudlabs <deepcloudlabs@gmail.com>
2020-11-27 10:26:51 +0300

    add get request /emplyees/:tckimlikno, /employee?page=0&size=25 add swagger ui

--- hr-backend/server.js
+++ hr-backend/server.js
@@ -4,6 +4,9 @@ let logger = require("morgan");
 let mongoose = require("mongoose");
 let util = require("./utils");
 
+let swaggerUi = require("swagger-ui-express");
+let openApiDoc = require("./swagger-hr-api.json");
+
 const port = 9001;
 const api = express();
 
@@ -14,6 +17,7 @@ api.use((req, res, next) => { // CORS
     res.header("Access-Control-Allow-Methods", "HEAD, POST, PUT, GET, DELETE, PATCH");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
+api.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openApiDoc));
 
 mongoose.connect(
     "mongodb://localhost:27017/hrdatabase",
@@ -89,6 +93,42 @@ const Employee = mongoose.model('employees', employeeSchema);
 // Fire Employee -> DELETE Employee
 // Increase Salary -> PUT/PATCH Employee
 // Query Employee -> GET Identity/Department/...
+
+//REST over HTTP -> Resource (Employee -> Aggregate)
+// 1. HTTP Method
+// 2. URL -> /employees
+// 3. Representation: "JSON"/"XML",
+//    HTTP Request Header -> "Accept: application/json" -> Response Body
+//                           "Content-Type: application/json" -> Request Body
+api.get("/hr/api/v1/employees/:tckimlikno", async (req,res) => {
+    let tckimlikno = req.params.tckimlikno;
+    try{
+        Employee.findOne(
+            {"identityNo": tckimlikno},
+            {"_id": false}
+        ).exec().then( emp => res.status(200).send(emp)); // Promise -> async
+    }catch(err){
+        res.status(404).send({"status": "employee is not found."});
+    }
+});
+
+// /hr/api/v1/employees?page=0&size=25
+api.get("/hr/api/v1/employees", (req,res) => {
+     let page = Number(req.query.page || 0);
+     let size = Number(req.query.size || 25);
+     let offset = page * size;
+     Employee.find(
+         {},
+         {"_id": false},
+         { "skip": offset , "limit": size},
+         async (err, employees) => {
+             if (err)
+                 res.status(404).send({"status": "Page not found"});
+             else
+                 res.status(200).send(employees);
+         }
+     )
+})
 
 let server = api.listen(port);
 console.log("REST Api is up and running @ " + port);
